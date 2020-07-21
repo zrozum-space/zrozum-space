1b2595a8be9d38c64dd693d6a08eca53080251f1
bmstefanski <bstefanskicom@gmail.com>
2020-07-21 15:21:21 +0200

    Fix netlifycms configuration

--- src/components/ResponsiveImage.js
+++ src/components/ResponsiveImage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Img from 'gatsby-image'
 
 const ResponsiveImage = ({ image, alt }) => {
-  return image.childImageSharp ? (
+  return image && image.childImageSharp ? (
     <Img fluid={image.childImageSharp.fluid} alt={alt} />
   ) : (
     <img style={{ display: 'table', maxWidth: '100%', margin: 'auto' }} src={require(`../img/${image}`)} alt={alt} />
